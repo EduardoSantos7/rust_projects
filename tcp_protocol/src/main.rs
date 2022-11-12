282aabf269b828371176cca59b136c58669781f5
Eduardo Santos Delgado <eduardosa@microsoft.com>
2022-11-12 08:59:06 -0800

    base of read packages

---
+++ tcp_protocol/src/main.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("read {} bytes: {:x}", nbytes, &buf[..nbytes]);
+    Ok(())
+}
