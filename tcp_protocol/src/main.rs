7ef6d0625e7f87f760f9f9d3fe50b459421e909b
Eduardo Santos Delgado <eduardosa@microsoft.com>
2023-03-12 22:22:51 -0700

    using  a parser

--- tcp_protocol/src/main.rs
+++ tcp_protocol/src/main.rs
@@ -57,3 +57,34 @@ fn example_read_package_loop_parsing_flags_protocol_ignore_no_ipv4() -> io::Resu
     }
     Ok(())
 }
+
+
+fn example_read_package_loop_using_etherparser_flags_protocol_ignore_no_ipv4() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_protocol = u16::from_be_bytes([buf[2], buf[3]]);
+
+        if eth_protocol != 0x0800 {
+            // no ipv4
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                // (src_ip, src_port, dst_ip, dst_port) -> quad
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                eprintln!(
+                    "{} -> {} {} bytes of protocol: {:x}", src, dst, p.payload_len(), proto);
+            },
+            Err(e) => {
+                eprintln!("ignoring weird packet {:?}", e);
+            }
+        }
+    }
+    Ok(())
+}
